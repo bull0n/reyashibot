f6af1588b428e58bc5bca7b19a9ea6736f496a71
bull0n <lucas.bulloni@protonmail.ch>
2022-09-29 15:03:48 +0200

    add the formatting of the glossary

--- lambda/tea_glossary_insert/handler/tea_glossary_insert.py
+++ lambda/tea_glossary_insert/handler/tea_glossary_insert.py
@@ -1,7 +1,9 @@
 import json
 import os
+from functools import reduce
 import boto3
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 SPREADSHEET_ID = os.environ['spreadsheet_id']
 API_KEY = os.environ['google_spreadsheet_api_key']
@@ -13,21 +15,34 @@ client = boto3.client('dynamodb')
 def get_spreadsheet():
     try:
         service = build('sheets', 'v4', developerKey=API_KEY)
-
         sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                    range=SAMPLE_RANGE).execute()
+        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=SAMPLE_RANGE).execute()
         values = result.get('values', [])
 
         if not values:
             print('No data found.')
 
-        print(values)
+        return format_spreadsheet(values)
     except HttpError as err:
         print(err)
 
+def split_entry(entry):
+    keys = entry[0].split(", ")
+    definition = entry[1]
+
+    return list(map(lambda k: {'word': k, 'definition': definition}, keys))
+
+def format_spreadsheet(values):
+    glossary = []
+
+    for row in values:
+        if len(row) > 1:
+            glossary.append(split_entry(row))
+
+    return glossary
+
 def insert_spreadsheet(spreadsheet):
-    print('hello')
+    print('bulk insert in dynamodb')
 
 def lambda_handler(event, context):
     spreadsheet = get_spreadsheet()
@@ -37,3 +52,6 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps(f'{API_KEY}:{SPREADSHEET_ID}')
     }
+
+if __name__ == "__main__":
+    print(lambda_handler(None, None))
