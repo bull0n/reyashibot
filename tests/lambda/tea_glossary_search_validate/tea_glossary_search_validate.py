5f39ca2754c38cf3a0f42a3f247ad180f9839542
bull0n <lucas.bulloni@protonmail.ch>
2024-11-05 13:15:03 +0100

    create a proper lifecycle event

--- tests/lambda/tea_glossary_search_validate/tea_glossary_search_validate.py
+++ tests/lambda/tea_glossary_search_validate/tea_glossary_search_validate.py
@@ -1,13 +1,27 @@
 import boto3
 import os
 
-client = boto3.client('lambda', endpoint_url='http://host.docker.internal:3001')
+lambda_client = boto3.client('lambda')
 LAMBDA_NAME = os.environ['LAMBDA_NAME']
+codedeploy_client = boto3.client('codedeploy')
 
 with open('event.json') as f: PAYLOAD = f.read()
 
 
 def lambda_handler(event, context):
-    response = client.invoke(FunctionName=LAMBDA_NAME, InvocationType='RequestResponse', Payload=PAYLOAD)
+    print(event)
+    try:
+        response = lambda_client.invoke(FunctionName=LAMBDA_NAME, InvocationType='RequestResponse', Payload=PAYLOAD)
+    except:
+        return codedeploy_client.put_lifecycle_event_hook_execution_status(
+        deploymentId=event['DeploymentId'],
+        lifecycleEventHookExecutionId=event['LifecycleEventHookExecutionId'],
+        status='Failed'
+    )
+    
 
-    return True
+    return codedeploy_client.put_lifecycle_event_hook_execution_status(
+        deploymentId=event['DeploymentId'],
+        lifecycleEventHookExecutionId=event['LifecycleEventHookExecutionId'],
+        status='Succeeded'
+    )
