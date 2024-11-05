d5673977310e7250b2c457eadd0d8afd05c023ab
bull0n <lucas.bulloni@protonmail.ch>
2024-11-05 13:34:49 +0100

    fix lambda for testing the search feature

--- tests/lambda/tea_glossary_search_validate/tea_glossary_search_validate.py
+++ tests/lambda/tea_glossary_search_validate/tea_glossary_search_validate.py
@@ -2,23 +2,21 @@ import boto3
 import os
 
 lambda_client = boto3.client('lambda')
-LAMBDA_NAME = os.environ['LAMBDA_NAME']
 codedeploy_client = boto3.client('codedeploy')
 
+LAMBDA_NAME = os.environ['LAMBDA_NAME']
 with open('event.json') as f: PAYLOAD = f.read()
-
 
 def lambda_handler(event, context):
     print(event)
     try:
-        response = lambda_client.invoke(FunctionName=LAMBDA_NAME, InvocationType='RequestResponse', Payload=PAYLOAD)
-    except:
-        return codedeploy_client.put_lifecycle_event_hook_execution_status(
-        deploymentId=event['DeploymentId'],
-        lifecycleEventHookExecutionId=event['LifecycleEventHookExecutionId'],
-        status='Failed'
-    )
-    
+        lambda_client.invoke(FunctionName=LAMBDA_NAME, InvocationType='RequestResponse', Payload=PAYLOAD)
+    except e:
+        codedeploy_client.put_lifecycle_event_hook_execution_status(
+            deploymentId=event['DeploymentId'],
+            lifecycleEventHookExecutionId=event['LifecycleEventHookExecutionId'],
+            status='Failed'
+        )
 
     return codedeploy_client.put_lifecycle_event_hook_execution_status(
         deploymentId=event['DeploymentId'],
